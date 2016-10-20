412e6db149c323b164dcc39860ab4de8f7860223
Xiang Li <lx881219@gmail.com>
2016-10-20 14:13:28 -0500

    change on members.html and fix from date

--- gmail/forms.py
+++ gmail/forms.py
@@ -5,5 +5,5 @@ from django import forms
 
 class GmailFilter(forms.Form):
     max_results = forms.IntegerField(label='Max Results', max_value=32767, initial=100)
-    from_date = forms.DateField(label='From Date', widget=forms.SelectDateWidget())
+    from_date = forms.DateField(label='From Date', widget=forms.SelectDateWidget(years=range(datetime.date.today().year-10, datetime.date.today().year)))
     to_date = forms.DateField(label='To Date', widget=forms.SelectDateWidget(), initial=datetime.date.today())
